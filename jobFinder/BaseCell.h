8469b44ea4d358cf94b3b2deba10b8dd895376a0
Mario Greco <mrgreco3@gmail.com>
2011-10-18 14:42:03 +0200

    Ristrutturazione interfaccia delle classi celle con fix del relativo riuso.

---
+++ jobFinder/BaseCell.h
@@ -0,0 +1,13 @@
+//
+//  BaseCell.h
+//  jobFinder
+//
+//  Created by mario greco on 18/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseCell : UITableViewCell
+
+@end

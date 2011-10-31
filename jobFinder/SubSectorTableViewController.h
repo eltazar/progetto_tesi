43802ea9a510117959130661799a60dde5b707c4
Mario Greco <mrgreco3@gmail.com>
2011-10-31 17:18:17 +0100

    SubSectorTableViewController: aggiunto controller per le sotto categorie di lavori. passa il settore scelto al delegato ora

---
+++ jobFinder/SubSectorTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SubSectorTableViewController.h
+//  jobFinder
+//
+//  Created by mario greco on 31/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SectorTableViewController.h"
+
+@interface SubSectorTableViewController : SectorTableViewController
+
+@end

35ee574e5e1db0b98bac895213c7f0b364b41b9e
Mario Greco <mrgreco3@gmail.com>
2011-11-19 22:50:44 +0100

    BaseCell: eliminati metodi che non sono della classe

--- jobFinder/BaseCell.m
+++ jobFinder/BaseCell.m
@@ -64,19 +64,4 @@
 }
 */
 
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
-#pragma  mark - Memory Management
-- (void)didReceiveMemoryWarning
-{
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
-}
 @end

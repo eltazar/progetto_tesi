cda21681c7a28d8ee20f5c28b9e370417b6057aa
Mario Greco <mrgreco3@gmail.com>
2011-11-23 20:23:02 +0100

    Utilities: creata classe contenente metodi di utilità varia

---
+++ jobFinder/Utilities.h
@@ -0,0 +1,13 @@
+//
+//  Utilities.h
+//  jobFinder
+//
+//  Created by mario greco on 23/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utilities : NSObject
+
+@end
---
+++ jobFinder/Utilities.m
@@ -0,0 +1,23 @@
+//
+//  Utilities.m
+//  jobFinder
+//
+//  Created by mario greco on 23/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Utilities.h"
+
+@implementation Utilities
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end

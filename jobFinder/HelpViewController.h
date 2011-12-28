3e16972b25736bcea5bed82b33c5416af7014929
Mario Greco <mrgreco3@gmail.com>
2011-12-28 20:06:00 +0100

    HelpViewController: UPDATE 1.0.1 inserita interfaccia help

---
+++ jobFinder/HelpViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelpViewController.h
+//  jobFinder
+//
+//  Created by mario greco on 28/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelpViewController : UIViewController
+
+@end

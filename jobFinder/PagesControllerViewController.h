a4eb63089b03769a64005812701fc3e9ede69f9f
Mario Greco <mrgreco3@gmail.com>
2011-12-31 13:52:24 +0100

    PagesControllerViewController: inserite pagine per l'help

---
+++ jobFinder/PagesControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PagesControllerViewController.h
+//  jobFinder
+//
+//  Created by mario greco on 28/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PagesControllerViewController : UIViewController
+
+@end

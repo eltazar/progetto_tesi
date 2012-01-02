cc4831c30b3b801130c6e8e1045f3863bbbbf365
Mario Greco <mrgreco3@gmail.com>
2012-01-02 19:08:52 +0100

    InformationSectionViewController: view continente il menu per le configurazioni

---
+++ jobFinder/InformationSectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  InformationSectionViewController.h
+//  jobFinder
+//
+//  Created by mario greco on 31/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InformationSectionViewController : UITableViewController
+
+@end
